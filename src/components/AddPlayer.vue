14286a3f058c72e4fa259ced94efe7cbe61d3b08
Ida <ida.nacka@gmail.com>
2023-04-11 14:47:52 +0200

    Enter

---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const x = ref("");
+const o = ref("");
+
+defineEmits(["addPlayers"]);
+
+</script>
+
+<template>
+    <form>
+        <label for="x">Player1</label>
+        <input v-model="x">
+        <label for="o">Player2</label>
+        <input v-model="o">
+        <button @click="$emit('addPlayers', x, o)">Save</button>
+    </form>
+</template>
+
+<style scoped></style>
---
+++ src/components/TicTacToe.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Player } from "../models/Player";
+import AddPlayers from "../components/AddPlayer.vue"
+
+let show = true;
+const players = ref<Player[]>(JSON.parse(localStorage.getItem("players") || "[]"));
+
+function saveToLs(players: Player[]) {
+    localStorage.setItem("players", JSON.stringify(players));
+}
+
+const addPlayers = (player1: string, player2: string) => {
+    players.value.push(new Player(player1, "x"));
+    players.value.push(new Player(player2, "o"));
+    saveToLs(players.value);
+
+}
+
+</script>
+
+
+<template>
+    <AddPlayers @add-players="addPlayers"></AddPlayers>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</template>
+
+<style scoped></style>
