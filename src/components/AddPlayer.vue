86bcf9825b7cf2b6e0c8b7e512eb31c3e1c7ab01
Ida <ida.nacka@gmail.com>
2024-04-24 18:21:56 +0200

    updated styling for add players

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const x = ref("");
 const o = ref("");
+
+const isSaveDisabled = computed(() => {
+  return x.value === "" || o.value === "";
+});
 
 defineEmits(["addPlayers"]);
 
@@ -11,12 +15,58 @@ defineEmits(["addPlayers"]);
 <template>
     <form>
         <h1>TicTacToe</h1>
+    <div class="input-container">
+      <div class="input-field">
         <label for="x">Player X</label>
-        <input v-model="x">
+        <input class="input" v-model="x">
+      </div>
+      <div class="input-field">
         <label for="o">Player O</label>
-        <input v-model="o">
-        <button @click="$emit('addPlayers', x, o)">Save</button>
+        <input class="input" v-model="o">
+      </div>
+    </div>
+        <button class="save-button" :disabled="isSaveDisabled" @click="$emit('addPlayers', x, o)">Save</button>
     </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.input-container {
+    margin-top: -15px;
+    display: flex;
+    flex-direction: column;
+}
+
+.input-field {
+    display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+}
+
+.input-field label {
+  text-align: left;
+  margin-bottom: 5px;
+  font-size: 1.2em;
+  font-weight: bold;
+}
+
+.input {
+    width: 250px;
+    padding: 15px 20px;
+    border-radius: 6px;
+    background-color: rgb(239, 242, 246);
+    text-decoration: none;
+}
+
+.save-button {
+    margin-top: 10px;
+    width: 180px;
+    padding: 13px;
+    background-color: rgb(175, 179, 198);
+}
+
+.save-button:disabled {
+  background-color: rgb(216, 218, 224); 
+}
+
+</style>
